5e1916f1645f17a8623872d7cdda260f023392f5
HaroldDRF <95robots@gmail.com>
2020-04-22 14:36:11 -0700

    score added

--- js/canvas.js
+++ js/canvas.js
@@ -61,13 +61,24 @@ function showCoords(event) {
     return mouse;
 }
 
+/*Mouse event*/
 canvas.addEventListener('click', function(evt) {
     let mouse = showCoords(evt);
-    switch (showCoords(evt)) {
-        case mouse.x <= square_2.x:
-            console.log("this is square 2");
-    }
+    console.log(mouse.x, mouse.y);
 
+    if (mouse.x > square_1.x && mouse.y > square_1.y && mouse.x < square_1.w && mouse.y < square_1.h) {
+        console.log("this is square 1");
+        square_1.Add_Score();
+        console.log("square 1: " + square_1.score);
+    } else if ((mouse.x > square_2.x && mouse.y > square_2.y && mouse.x < square_2.w + square_2.x && mouse.y < square_2.h + square_2.y)) {
+        console.log("this is square 2");
+        square_2.Add_Score();
+        console.log("square 2: " + square_2.score);
+    } else if ((mouse.x > square_3.x && mouse.y > square_3.y && mouse.x < square_3.w + square_3.x && mouse.y < square_3.h + square_3.y)) {
+        console.log("this is square 3");
+        square_3.Add_Score();
+        console.log("square 3: " + square_3.score);
+    }
 })
 
 
