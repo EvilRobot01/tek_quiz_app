f56281ee397e9a861ae2d96c80628ee4326c5c49
HaroldDRF <95robots@gmail.com>
2020-04-26 14:13:01 -0700

    character screen

--- js/canvas.js
+++ js/canvas.js
@@ -3,13 +3,16 @@ const ctx = canvas.getContext("2d");
 
 let score;
 let highscore;
+let menu;
 let gameScreen;
+let warning_borders;
+let character_screen;
 let gravity;
 let obstacles;
 let gameSpeed;
 let keys = [];
 
-const gameState = { Menu: 0, GameScreen: 1, GameOver: 1, CharacterShow: 2 };
+const gameState = { Menu: 0, GameScreen: 1, GameOver: 2, CharacterShow: 3, WarningBorders: 4 };
 let currentState = gameState.Menu;
 
 function getState() {
@@ -26,17 +29,16 @@ class Square {
         this.w = w;
         this.h = h;
         this.c = c;
-
+        this.show_pic;
         this.score = 0;
     }
 
-    /*Animate() {
-        if () {
-
-        } else {
-
-        }
-    }*/
+    Show_() {
+        this.show_pic = true;
+    }
+    Hide_() {
+        this.hide_pic = false;
+    }
     Add_Score() {
         this.score++;
     }
@@ -50,6 +52,12 @@ class Square {
     }
 }
 
+menu = new Square(canvas.width / 3, canvas.height / 3, canvas.width / 3, canvas.height / 3, 'black');
+
+warning_borders = [new Square(0, 0, canvas.width, canvas.height / 10, 'red'),
+    new Square(0, canvas.height - canvas.height / 10, canvas.width, canvas.height / 10, 'red')
+];
+
 gameScreen = [new Square(0, 0, canvas.width / 3, canvas.height / 2 - 5, 'black'),
     new Square(canvas.width / 3 + 5, 0, canvas.width / 3 - 5, canvas.height / 2 - 5, 'black'),
     new Square(canvas.width - canvas.width / 3 + 5, 0, canvas.width / 3, canvas.height / 2 - 5, 'black'),
@@ -58,19 +66,26 @@ gameScreen = [new Square(0, 0, canvas.width / 3, canvas.height / 2 - 5, 'black')
     new Square(canvas.width - canvas.width / 3 + 5, canvas.height / 2, canvas.width / 3, canvas.height / 2, 'black')
 ];
 
-let menu = new Square(canvas.width / 3, canvas.height / 3, canvas.width / 3, canvas.height / 3, 'black');
+character_screen = [new Square(0, 0, canvas.width / 3 + 30, canvas.height / 2 - 5 + 30, 'white'),
+    new Square(canvas.width / 3 + 5 - 15, 0, canvas.width / 3 - 5 + 30, canvas.height / 2 - 5 + 30, 'white'),
+    new Square(canvas.width - canvas.width / 3 + 5 - 30, 0, canvas.width / 3 + 30, canvas.height / 2 - 5 + 30, 'white'),
+    new Square(0, canvas.height / 2 - 30, canvas.width / 3 + 30, canvas.height / 2 + 30, 'white', 0),
+    new Square(canvas.width / 3 + 5 - 15, canvas.height / 2 - 30, canvas.width / 3 - 5 + 30, canvas.height / 2 + 30, 'white'),
+    new Square(canvas.width - canvas.width / 3 + 5 - 30, canvas.height / 2 - 30, canvas.width / 3 + 30, canvas.height / 2 + 30, 'white')
+];
 
 
-function showCoords(evt) {
+function setCoord(evt) {
     let mouse = new Object;
     mouse.x = event.clientX;
     mouse.y = event.clientY;
 
-    return mouse;
+    return mouse.x > evt.x && mouse.y > evt.y && mouse.x < evt.w + evt.x && mouse.y < evt.h + evt.y;
 }
 
-function setCoord(evt) {
-    return showCoords(evt).x > evt.x && showCoords(evt).y > evt.y && showCoords(evt).x < evt.w + evt.x && showCoords(evt).y < evt.h + evt.y;
+
+function showCharacer(evt) {
+
 }
 
 //Mouse event
@@ -85,15 +100,23 @@ canvas.addEventListener('click', function(evt) {
     }
 
     //Game Screen
-    if (currentState == gameState.GameScreen) {
+    if (currentState == gameState.GameScreen || currentState == gameState.WarningBorders) {
         if (setCoord(gameScreen[0])) {
             console.log("this is gameScreen[0]");
             gameScreen[0].Add_Score();
             console.log("gameScreen[0]: " + gameScreen[0].score);
+            if (gameScreen[0].score == 5) {
+                currentState = gameState.WarningBorders;
+            }
         } else if (setCoord(gameScreen[1])) {
             console.log("this is gameScreen[1]");
             gameScreen[1].Add_Score();
             console.log("gameScreen[1]: " + gameScreen[1].score);
+            if (gameScreen[1].score == 5) {
+                character_screen[0].show_pic = true;
+                character_screen[1].show_pic = true;
+                character_screen[2].show_pic = true;
+            }
         } else if (setCoord(gameScreen[2])) {
             console.log("this is gameScreen[2]");
             gameScreen[2].Add_Score();
@@ -110,9 +133,9 @@ canvas.addEventListener('click', function(evt) {
             console.log("this is gameScreen[5]");
             gameScreen[5].Add_Score();
             console.log("gameScreen[5]: " + gameScreen[5].score);
-
         }
     }
+
 })
 
 
@@ -141,10 +164,23 @@ function Update() {
     }
     if (currentState == gameState.GameScreen) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (let items in gameScreen) {
-            gameScreen[items].Draw();
+        for (let item in gameScreen) {
+            gameScreen[item].Draw();
         }
     }
+    if (currentState == gameState.WarningBorders) {
+        for (let item in warning_borders) {
+            warning_borders[item].Draw();
+        }
+    }
+    for (let item in character_screen) {
+
+        if (character_screen[item].show_pic == true) {
+            character_screen[item].Draw();
+        }
+
+    }
+
 
 }
 
